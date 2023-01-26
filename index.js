1489ce1db5f55fd6127cbc7d8d7bfbec0407b1f6
Carlos Ramirez <carlosrv125@icloud.com>
2023-01-26 14:32:53 -0500

    test

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { createClient } from "redis";
+
+const app = express();
+const port = 3000;
+const redisHost = process.env.REDIS_HOST ? process.env.REDIS_HOST : "";
+const redisPort = process.env.REDIS_PORT ? process.env.REDIS_PORT : "";
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
+
+const client = createClient({
+  url: `redis://${redisHost}:${redisPort}`,
+});
+
+app.get("/:key", async (req, res) => {
+  const value = await client.get(req.params.key);
+  res.status(200).send({
+    result: value,
+  });
+});
+
+app.listen(port, async () => {
+  await connectRedis();
+  console.log(`Example app listening on port ${port}`);
+});
+
+const connectRedis = async () => {
+  await client.connect();
+  await client.set("carlos", "alicita");
+  await client.set("alicita", "carlos");
+  client.on("error", (err) => console.log("Redis Client Error", err));
+};
